6dd32889e50eb98fe39697b27f1de736672d8a4c
st976072 <63726256+st976072@users.noreply.github.com>
2020-11-03 20:11:12 +0530

    I Want my this project to be always in reality

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+    $username = $_GET["username"];
+    $password = $_GET["password"];
+
+    echo "<table border='2'>
+    <tr><td>UserName:</td><td>$username</tr><tr><td>Password:</td><td>$password</td></tr><br>";
+?>
